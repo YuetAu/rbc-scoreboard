e6f2981b9721bf22ac632f37c64d68f534abc0a8
YuetAu <sam07@live.hk>
2024-09-06 17:53:50 +0800

    disable dynamic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,7 @@
-import { Metadata } from 'next';
-import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 
-export const runtime = 'edge';
 
 export default function Home() {
     redirect(`/dashboard`)
     return (<></>)
-}
-
-export async function generateMetadata(): Promise<Metadata> {
-    const headersList = headers();
-    const origin = headersList.get('host');
-    console.log(origin);
-    let metadata: Metadata = {
-        metadataBase: new URL(`https://${origin}`),
-        openGraph: {
-            images: origin?.includes("ustrobocon.win") ? `/og/HKUSTRobocon.svg` : `/og/YuetAuRobocon.svg`,
-        }
-    };
-    return metadata;
 }
