7c30a2ecd168898cde1dc5dc6b5595fbfbb7daf6
YuetAu <me@yuetau.net>
2024-09-10 23:09:48 +0800

    helper button

--- src/app/props/dashboard/Counter.tsx
+++ src/app/props/dashboard/Counter.tsx
@@ -4,6 +4,8 @@ import { Box, Flex } from "@chakra-ui/react";
 export function Counter(props: any) {
     return (
         <Flex>
+
+
             <Box shadow={"lg"} rounded={"lg"} px={"0.5rem"} style={{
                 fontSize: props.small ? "1.7rem" : "2rem",
                 textAlign: "center",
@@ -14,11 +16,49 @@ export function Counter(props: any) {
                 userSelect: "none",
                 cursor: "pointer",
             }}
-                onClick={(e) => { e.preventDefault(); props.setCounter(props.counter + 1); }}
-                onContextMenu={(e) => { props.disableLeftClick ? e.preventDefault() : e.preventDefault(); props.setCounter(props.counter - 1 > 0 ? props.counter - 1 : 0); }}
+                onClick={(e) => { props.smDevice ? e.preventDefault() : e.preventDefault(); props.setCounter(props.counter + 1); }}
+                onContextMenu={(e) => { props.disableLeftClick || props.smDevice ? e.preventDefault() : e.preventDefault(); props.setCounter(props.counter - 1 > 0 ? props.counter - 1 : 0); }}
             >
                 {props.counter}
+                {props.smDevice && (<>
+                    <Box
+                        position={"absolute"}
+                        top={"0.3rem"}
+                        left={"-2.5rem"}
+                        shadow={"md"} rounded={"sm"} style={{
+                            fontSize: "1.7rem",
+                            textAlign: "center",
+                            lineHeight: "2rem",
+                            backgroundColor: "white",
+                            color: "black",
+                            width: "2rem",
+                            userSelect: "none",
+                            cursor: "pointer",
+                        }}
+                        onClick={(e) => { e.preventDefault(); props.setCounter(props.counter - 1); }}
+                    >
+                        {"-"}
+                    </Box>
+                    <Box
+                        position={"absolute"}
+                        top={"0.3rem"}
+                        left={"3rem"}
+                        shadow={"md"} rounded={"sm"} style={{
+                            fontSize: "1.7rem",
+                            textAlign: "center",
+                            lineHeight: "2rem",
+                            backgroundColor: "white",
+                            color: "black",
+                            width: "2rem",
+                            userSelect: "none",
+                            cursor: "pointer",
+                        }}
+                        onClick={(e) => { e.preventDefault(); props.setCounter(props.counter + 1); }}
+                    >
+                        {"+"}
+                    </Box>
+                </>)}
             </Box>
-        </Flex>
+        </Flex >
     )
 }
