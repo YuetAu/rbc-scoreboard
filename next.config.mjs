c2b7f327b53245f9834c45826e81c6dd31faa50a
YuetAu <sam07@live.hk>
2024-09-16 10:07:55 +0800

    remove pre cache rules

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,10 @@
-import crypto from "node:crypto";
-import fs from "node:fs";
-import path from "node:path";
+
 import withSerwistInit from "@serwist/next";
-import { glob } from "glob";
-
-const publicScan = glob.sync(["**/*"], {
-  nodir: true,
-  cwd: "public",
-  ignore: ["swe-worker-*.js", "sw.js", "sw.js.map"],
-});
-
-/**
- * @param {string} filePath
- * @returns
- */
-const getFileHash = (filePath) => crypto.createHash("md5").update(fs.readFileSync(filePath)).digest("hex");
 
 const withSerwist = withSerwistInit({
   swSrc: "src/app/sw.ts",
   swDest: "public/sw.js",
   cacheOnNavigation: true,
-  additionalPrecacheEntries: [
-    { url: "/manifest.json", revision: getFileHash("src/app/manifest.json") },
-    ...publicScan.map((f) => ({
-      // Replace "/" with your `nextConfig.basePath` if applicable.
-      url: path.posix.join("/", f),
-      revision: getFileHash(path.join("public", f)),
-    })),
-  ],
 });
 
 export default withSerwist({
