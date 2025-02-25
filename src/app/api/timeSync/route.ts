cdbad7c2bd79194d6f9a171b7274ba0f00e81ee9
YuetAu <sam07@live.hk>
2025-02-25 12:22:42 +0800

    push to fix timeSync

--- src/app/api/timeSync/route.ts
+++ src/app/api/timeSync/route.ts
@@ -1,7 +1,5 @@
 export const runtime = "edge";
-import { unstable_noStore as noStore } from "next/cache";
 
 export async function GET(request: Request) {
-    noStore();
     return new Response(Date.now().toString());
 }
